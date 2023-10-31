1005183221be7e63399f3eab6ea11c189f8faa23
brian.chao <kuroiwaruiko21@gmail.com>
2023-10-31 17:46:31 +0800

    modify static path with absolute path

--- server/src/index.ts
+++ server/src/index.ts
@@ -60,7 +60,7 @@ app.use(
 );
 app.use('/api/auth', authRoutes);
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 // app.get('*', (req, res) =>
 //     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
 // );
